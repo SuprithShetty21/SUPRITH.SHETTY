d9f0fe20394147c0bcdf5653e76ccbdefe6eecbf
Suprith Shetty <39983712+SuprithShetty21@users.noreply.github.com>
2019-10-30 17:14:47 +0530

    Update suprithshetty1.c
    the computation and checking part of the function has been written as separate functions.

--- suprithshetty1.c
+++ suprithshetty1.c
@@ -1,21 +1,43 @@
     #include <stdio.h>
     
+    int computeArmstrong(int); 
+    void checkArmstrong(int, int);
+
     int main()
     {
         int n, temp_n, remndr, out = 0;
         printf("Enter a three digit integer: ");
         scanf("%d", &n);
-        temp_n = n;
-        while (temp_n != 0)                      //computing on loop whether the given number is an armstrong number or not
+
+        out = computeArmstrong(n);
+        checkArmstrong(n, out);
+        return 0;
+    }
+    
+
+
+int computeArmstrong(int n)
+{
+    int temp_n, remndr, out = 0;
+       
+    temp_n = n;
+    
+    while (temp_n != 0)                      //computing on loop whether the given number is an armstrong number or not
         {
             remndr = temp_n%10;
             out += remndr*remndr*remndr;
             temp_n /= 10;
         }
+    
+    return out;
+}
+
+
+
+void checkArmstrong(int n, int out)
+{
         if(out == n)                                          //checking whether the given number is an armstrong number or not
             printf("%d is an Armstrong number.",n);
         else
             printf("%d is not an Armstrong number.",n);
-        return 0;
-    }
-    
+}
