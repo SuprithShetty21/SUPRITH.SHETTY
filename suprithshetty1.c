259ddc7fcf4770123e6d72cc4079e57e6d3103e0
Suprith Shetty <39983712+SuprithShetty21@users.noreply.github.com>
2019-10-30 17:02:03 +0530

    Update suprithshetty1.c

--- suprithshetty1.c
+++ suprithshetty1.c
@@ -6,13 +6,13 @@
         printf("Enter a three digit integer: ");
         scanf("%d", &n);
         temp_n = n;
-        while (temp_n != 0)
+        while (temp_n != 0)                      //computing on loop whether the given number is an armstrong number or not
         {
             remndr = temp_n%10;
             out += remndr*remndr*remndr;
             temp_n /= 10;
         }
-        if(out == n)
+        if(out == n)                                          //checking whether the given number is an armstrong number or not
             printf("%d is an Armstrong number.",n);
         else
             printf("%d is not an Armstrong number.",n);
